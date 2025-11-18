b5f680a99c375d005d8f5cc51cde68cdef4a5ecc
Mahmoud <mahmoudnasser1561@gmail.com>
2025-11-18 21:52:08 +0200

    applied fmt

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -17,3 +17,23 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "ecs" {
+  name        = "${var.project_name}-ecs-sg"
+  description = "Security group for ECS tasks"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
