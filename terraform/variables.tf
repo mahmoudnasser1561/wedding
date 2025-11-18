da380c10ad6459a2a0c426e14e5f44b542aba9f2
Mahmoud <mahmoudnasser1561@gmail.com>
2025-11-18 20:09:12 +0200

    added variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,39 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "wedding-site"
+}
+
+variable "vpc_cidr" {
+    description = "CIDR block for the VPC"
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "container_cpu" {
+  description = "CPU units for the container"
+  type        = number
+  default     = 256
+}
+
+variable "container_memory" {
+  description = "Memory (MiB) for the container"
+  type        = number
+  default     = 512
+}
+
+variable "desired_count" {
+  description = "Number of ECS tasks to run"
+  type        = number
+  default     = 1
+}
