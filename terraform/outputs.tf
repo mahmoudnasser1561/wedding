07a9e59ee231a7ac0ed38761d2b3d167e6fe89b1
Mahmoud <mahmoudnasser1561@gmail.com>
2025-11-18 22:09:46 +0200

    outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "ecr_repository_url" {
+  description = "URL of the ECR repository"
+  value       = aws_ecr_repository.main.repository_url
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the ALB"
+  value       = aws_lb.main.dns_name
+}
+
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.main.name
+}
+
+output "ecs_service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.main.name
+}
