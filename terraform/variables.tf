a2f45fb5c7ae7acada7861438c74dafdfc712ae3
Mahmoud <mahmoudnasser1561@gmail.com>
2025-11-18 20:19:04 +0200

    added the VPC

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,9 +11,9 @@ variable "project_name" {
 }
 
 variable "vpc_cidr" {
-    description = "CIDR block for the VPC"
-    type = string
-    default = "10.0.0.0/16"
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnets" {
---
+++ terraform/vpc.tf
@@ -0,0 +1,31 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-subnet-${count.index + 1}"
+  }
+}
